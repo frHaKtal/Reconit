7f372378df94eee5c65701d87122a07c815572f2
_frHaKtal_ <49241224+frHaKtal@users.noreply.github.com>
2025-02-20 00:00:45 +0100

    Update enum_task.py

--- enum_task.py
+++ enum_task.py
@@ -3,40 +3,37 @@ import sqlite3
 import requests
 import socket
 import subprocess
-from selenium import webdriver
-from selenium.webdriver.chrome.options import Options
-from selenium.webdriver.common.by import By
-from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.support import expected_conditions as EC
-from bs4 import BeautifulSoup
-from PIL import Image
-import io
-import imagehash
+from playwright.sync_api import sync_playwright
+from concurrent.futures import ThreadPoolExecutor
+import concurrent.futures
 import tldextract
 import re
+import imagehash
+import io
+from PIL import Image
+import json
 
 # Connexion à la base de données
-#conn = sqlite3.connect('database.db')
-#cursor = conn.cursor()
-
 def get_db_connection():
     return sqlite3.connect('database.db')
 
-def get_phash(screenshot_base64):
+def execute_command(command):
+    """Exécute une commande shell de manière sécurisée et retourne la sortie."""
     try:
-        # Décoder l'image en base64 pour obtenir les bytes
-        image_data = base64.b64decode(screenshot_base64)
+        result = subprocess.run(command, capture_output=True, text=True, check=True)
+        return result.stdout.strip().split('\n')
+    except subprocess.CalledProcessError as e:
+        return []
 
-        # Créer une image PIL à partir des bytes
-        image = Image.open(io.BytesIO(image_data))
+def get_spfdmarc(domain):
+    """Vérifie la présence des enregistrements SPF et DMARC pour un domaine."""
+    spf_records = execute_command(["dig", "TXT", domain, "+short"])
+    dmarc_records = execute_command(["dig", "TXT", f"_dmarc.{domain}", "+short"])
 
-        # Calculer le perceptual hash
-        phash_value = str(imagehash.phash(image))
-        return phash_value
+    spf_check = "✔️" if any("v=spf1" in record for record in spf_records) else "❌"
+    dmarc_check = "✔️" if any("v=DMARC1" in record for record in dmarc_records) else "❌"
 
-    except Exception as e:
-        #print(f"Failed to calculate phash: {e}")
-        return None
+    return f"{spf_check} {dmarc_check}"
 
 def get_method(domain):
     # Tester HTTPS
@@ -60,89 +57,210 @@ def get_method(domain):
     return " | ".join(result) if result else "No methods found"
 
 
-def get_spfdmarc(domain):
+def get_httpx_data(domains):
+    """Récupère le statut HTTP, la méthode, le titre, l'IP et les technologies d'une liste de domaines."""
+    domain_results = {}
+    #driver = setup_selenium_driver()
+    domains_str = "\n".join(domains)
+#f"echo \"{domains_str}\" > file.txt | httpx --tech-detect --silent -nc -timeout 3 -l file.txt"
+    # Exécuter httpx sur la liste des domaines
+    result = subprocess.run(
+        f"echo \"{domains_str}\" > file.txt | httpx -ip -title -method -sc -td --tech-detect --silent -nc -timeout 3 -l file.txt",
+        shell=True,
+        stdout=subprocess.PIPE,
+        stderr=subprocess.PIPE,
+        text=True
+    )
 
-    #print(f"✔️  \033[1m{len(domains)}\033[0m domain find")
-    #spf = curl -s https://dmarcly.com/server/spf_check.php?domain={domain} | grep -q --color=auto "SPF record not found" && echo -e "[!]\e[31m SPF RECORD NOT FOUND\e[0m" || echo -e "[●]\e[33m SPF RECORD VALID\e[0m"
-    #dmarc = curl -s https://dmarcly.com/server/dmarc_check.php\?domain\={domain} | grep -q --color=auto "DMARC record not found" && echo -e "[!]\e[31m DMARC RECORD NOT FOUND\e[0m" || echo -e "[●]\e[33m DMARC RECORD VALID\e[0m"
-    url_spf = f"https://dmarcly.com/server/spf_check.php?domain={domain}"
-    response_spf = requests.get(url_spf)
-    if "SPF record not found" in response_spf.text:
-        #print("\033[31m[!]\033[0m SPF RECORD NOT FOUND")
-        spf_result = "❌"
-    else:
-        #print("\033[33m[●]\033[0m SPF RECORD VALID")
-        spf_result = "✔️ "
+    if not result.stdout.strip():
+        return {domain: None for domain in domains}
 
-    url_dmarc = f"https://dmarcly.com/server/dmarc_check.php?domain={domain}"
-    response_dmarc = requests.get(url_dmarc)
-    if "DMARC record not found" in response_dmarc.text:
-        #print("\033[31m[!]\033[0m DMARC RECORD NOT FOUND")
-        dmarc_result = "❌"
-    else:
-        #print("\033[33m[●]\033[0m DMARC RECORD VALID")
-        dmarc_result = "✔️ "
+    screenshots=take_screenshots_parallel(domains, max_workers=20)
+    for line in result.stdout.split("\n"):
+        match = re.search(r"(https?:\/\/[^\s]+) \[(\d+)\] \[(\w+)\] \[(.*?)\] \[(.*?)\] \[(.*?)\]", line)
+        if match:
+            full_url = match.group(1)
+            domain = match.group(1).replace("https://", "").replace("http://", "")
+            http_status = match.group(2)
+            #method = match.group(3)
+            method = get_method(domain)
+            title = match.group(4)
+            ip = match.group(5)
+            tech_list = match.group(6).split(", ") if match.group(6) else []
 
-    return spf_result + dmarc_result
+            domain_principal = re.search(r"([a-zA-Z0-9-]+\.[a-zA-Z]{2,})$", full_url)
+            #screenshot=take_screenshots_parallel(domain, max_workers=20)
+            screenshot = screenshots.get(domain, None)
+            domain_results[domain] = {
+                "http_status": http_status,
+                "method": method,
+                "title": title,
+                "ip": ip,
+                "tech_list": tech_list,
+                #"open_port": scan_naabu_fingerprint(domain),  # Scan des ports ouverts
+                "open_port": "xx",  # Scan des ports ouverts
+                "screen": screenshot,  # Capture d'écran
+                "phash": get_phash(screenshot),  # Perceptual hash de l'image
+                "spfdmarc": get_spfdmarc(domain_principal.group(1))  # SPF/DMARC info
+            }
 
-def get_ip(domain):
+    result = subprocess.run(
+        f"rm file.txt",
+        shell=True,
+        stdout=subprocess.PIPE,
+        stderr=subprocess.PIPE,
+        text=True
+    )
 
+    return domain_results
+
+
+
+def take_screenshot_base64(url):
+    """Prend un screenshot en base64 sans écrire de fichier."""
     try:
-        ip_address = socket.gethostbyname(domain)
-        return ip_address
-    except socket.error as e:
-        pass
-        #print(f"❌ Error resolving {domain_name}: {e}")
-    return None
-
-def get_http_status(domain):
-
-    url = f"http://{domain}"
-    try:
-        response = requests.get(url, timeout=10)  # Ajoute un timeout pour éviter de bloquer
-        return response.status_code
-    except requests.RequestException as e:
-        #print(f"❌ Error fetching {url}: {e}")
+        with sync_playwright() as p:
+            browser = p.chromium.launch(headless=True)
+            page = browser.new_page()
+            #print(url)
+            page.goto(f"http://{url}", timeout=5000)  # Timeout max de 10s
+            screenshot = page.screenshot()
+            browser.close()
+            return base64.b64encode(screenshot).decode('utf-8')
+    except Exception as e:
+        #print(f"[✘] Failed: {url} -> {e}")
         return None
 
-def get_techno(domain):
-    result = subprocess.run(f"echo {domain} | httpx --tech-detect --silent -nc | grep -oP '\\[.*?\\]'",
-                            shell=True,
-                            stdout=subprocess.PIPE,
-                            stderr=subprocess.PIPE,
-                            text=True)
 
-    if not result.stdout.strip():  # Vérifie si stdout est vide ou seulement des espaces
-        return None
-    clean_techs = re.findall(r'\[(.*?)\]', result.stdout.strip())
-    return ", ".join(clean_techs)  # Retourne une chaîne formatée
-    #return result.stdout.strip()  # Retire les espaces en trop si nécessaire
+def take_screenshots_parallel(urls, max_workers=20):
+    """Lance plusieurs screenshots en parallèle et retourne un dict avec les résultats."""
 
-def get_title(domain):
-    url = f"http://{domain}"
-    try:
-        response = requests.get(url, timeout=5)
-        if response.status_code == 200:
-            soup = BeautifulSoup(response.text, 'html.parser')
-            return soup.title.string if soup.title else 'No title found'
-        else:
-            return 'No title found'
-    except requests.RequestException as e:
-        pass
-        #print(f"❌ Error fetching {url}: {e}")
-        return 'No title found'
+    # 🔥 Vérifie que urls est bien une liste
+    if isinstance(urls, str):
+        urls = [urls]  # Convertit une chaîne en liste
+
+    results = {}
+    with ThreadPoolExecutor(max_workers=max_workers) as executor:
+        for url, screenshot in zip(urls, executor.map(take_screenshot_base64, urls)):
+            if screenshot:
+                results[url] = screenshot #modifier ici
+                #domain_results[domain_name]['screen'].append(screenshot_base64)
+
+    return results
+
+
+def update_db(program_name, domain_data):
+    """Ajoute ou met à jour les données du domaine dans la base de données."""
+
+    with sqlite3.connect("database.db") as conn:
+        cursor = conn.cursor()
+
+        # Récupérer l'ID du programme
+        cursor.execute("SELECT id FROM programs WHERE program_name = ?", (program_name,))
+        program_id = cursor.fetchone()
+
+        if not program_id:
+            print(f"⚠️ Erreur : Aucun programme trouvé pour '{program_name}'.")
+            return
+
+        program_id = program_id[0]
+
+        # Insérer les domaines et leurs détails
+        for domain, data in domain_data.items():
+            if data is None:
+                continue  # Si aucune donnée, on passe
+
+            # Insérer le domaine s'il n'existe pas
+            cursor.execute(
+                'INSERT OR IGNORE INTO domains (program_id, domain_name) VALUES (?, ?)',
+                (program_id, domain)
+            )
+
+            # Récupérer l'ID du domaine (nouvellement inséré ou existant)
+            cursor.execute(
+                'SELECT id FROM domains WHERE program_id = ? AND domain_name = ?',
+                (program_id, domain)
+            )
+            domain_id = cursor.fetchone()[0]
+
+            # Insérer les détails dans domain_details
+            cursor.execute('''
+                INSERT INTO domain_details
+                (domain_id, http_status, method, title, ip, techno, open_port, screen, phash, spfdmarc)
+                VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?)
+            ''', (
+                domain_id,
+                data["http_status"] if data["http_status"] else None,
+                data["method"] if data["method"] else None,
+                data["title"] if data["title"] else None,
+                data["ip"] if data["ip"] else None,
+                ", ".join(data["tech_list"]) if data["tech_list"] else None,
+                str(data["open_port"]) if data["open_port"] else None,
+                str(data["screen"]) if data["screen"] else None,
+                str(data["phash"]) if data["phash"] else None,
+                str(data["spfdmarc"]) if data["spfdmarc"] else None
+            ))
+
+        conn.commit()
+
+def update_dbb(program_name, domain_data):
+    """Ajoute ou met à jour les données du domaine dans la base de données."""
+    with sqlite3.connect("database.db") as conn:
+        cursor = conn.cursor()
+
+        # Récupérer l'ID du programme
+        cursor.execute("SELECT id FROM programs WHERE program_name = ?", (program_name,))
+        program_id = cursor.fetchone()
+
+        if not program_id:
+            print(f"⚠️ Erreur : Aucun programme trouvé pour '{program_name}'.")
+            return
+
+        program_id = program_id[0]
+
+        # Insérer les domaines et leurs détails
+        for domain, data in domain_data.items():
+            if data is None:
+                continue  # Si aucune donnée, on passe
+
+            cursor.execute(
+                'INSERT OR IGNORE INTO domains (program_id, domain_name) VALUES (?, ?)',
+                (program_id, domain)
+            )
+            domain_id = cursor.lastrowid  # ID du domaine inséré
+
+            # Insérer les détails dans domain_details
+            cursor.execute('''
+                INSERT INTO domain_details
+                (domain_id, http_status, method, title, ip, techno, open_port, screen, phash, spfdmarc)
+                VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?)
+            ''', (
+                domain_id,
+                data["http_status"],
+                data["method"],
+                data["title"],
+                data["ip"],
+                ", ".join(data["tech_list"]) if data["tech_list"] else None,
+                #", ".join(map(str, data["open_port"])) if data["open_port"] else None,
+                str(data["open_port"]) if data["open_port"] else None,
+                str(data["screen"]) if data["screen"] else None,
+                str(data["phash"]) if data["phash"] else None,
+                str(data["spfdmarc"]) if data["spfdmarc"] else None
+            ))
+
+        conn.commit()
+
 
 def scan_naabu_fingerprint(domain):
     try:
         # Exécuter la commande avec un timeout de 10 secondes
         result = subprocess.run(
-            f"naabu -host {domain} -ec -cdn -silent -s s 2>/dev/null | grep -oP '\d+(?=\s*$)' | tr '\n' ',' | sed 's/,$//'",
-#f"naabu -host {domain} -ec -cdn -silent 2>/dev/null | fingerprintx",
+            f"naabu -host {domain} -retries 1 -ec -silent -s s 2>/dev/null | grep -oP '\d+(?=\s*$)' | tr '\n' ',' | sed 's/,$//'",
             shell=True,
             stdout=subprocess.PIPE,
             stderr=subprocess.PIPE,
             text=True,
-            timeout=10  # Timeout de 10 secondes
+            timeout=5  # Timeout de 10 secondes
         )
 
         # Renvoyer la sortie de la commande (stdout)
@@ -156,131 +274,33 @@ def scan_naabu_fingerprint(domain):
         return None
 
 
-
-def check_protocol(domain_name):
-    """
-    Vérifie si le domaine est accessible via HTTPS ou HTTP.
-    Renvoie le protocole disponible.
-    """
+def get_phash(screenshot_base64):
     try:
-        response = requests.get(f"https://{domain_name}", timeout=5)
-        if response.status_code == 200:
-            return "https"
-    except requests.RequestException:
-        pass  # Ignore l'erreur et essaie HTTP ensuite
+        # Décoder l'image en base64 pour obtenir les bytes
+        image_data = base64.b64decode(screenshot_base64)
 
-    try:
-        response = requests.get(f"http://{domain_name}", timeout=5)
-        if response.status_code == 200:
-            return "http"
-    except requests.RequestException:
-        pass  # Si les deux échouent, retourne None
+        # Créer une image PIL à partir des bytes
+        image = Image.open(io.BytesIO(image_data))
 
-    return None
+        # Calculer le perceptual hash
+        phash_value = str(imagehash.phash(image))
+        return phash_value
 
-def take_screenshot(domain_name):
+    except Exception as e:
+        #print(f"Failed to calculate phash: {e}")
+        return None
+
+
+def ssetup_selenium_driver():
     chrome_options = Options()
     chrome_options.add_argument("--headless")
     chrome_options.add_argument("--no-sandbox")
     chrome_options.add_argument("--disable-dev-shm-usage")
     chrome_options.add_argument("--log-level=3")
-    driver = webdriver.Chrome(options=chrome_options)
-
-    try:
-        # Ouvrir la page du domaine
-        protocol = check_protocol(domain_name)
-        #driver.get(f"http://{domain_name}")
-        driver.get(f"{protocol}://{domain_name}")
-        screenshot = driver.get_screenshot_as_png()
-
-        # Encoder en base64
-        screenshot_base64 = base64.b64encode(screenshot).decode('utf-8')
-        return screenshot_base64
-
-    except Exception as e:
-        #print(f"Failed to take screenshot for {domain_name}: {e}")
-        return None
-    finally:
-        driver.quit()
-
-def add_dom(program_name, domain):
-    # Ouvrir une nouvelle connexion et un curseur pour ce thread
-    conn = sqlite3.connect('database.db')
-    cursor = conn.cursor()
-    #conn = get_db_connection()
-    #cursor = conn.cursor()
-
-    try:
+    return webdriver.Chrome(options=chrome_options)
 
 
-        cursor.execute('SELECT id FROM domains WHERE domain_name = ?', (domain,))
-        existing_domain = cursor.fetchone()
-
-        if existing_domain:
-            print(f"⚠️  Domain '{domain}' already exists.")
-            return
-
-        #print(f"Adding domain: {domain} to program: {program_name}")
-
-        # Récupérer les informations d'énumération
-        ip = get_ip(domain)
-        open_ports = scan_naabu_fingerprint(domain)
-        #spfdmarc = get_spfdmarc(domain)
-        extracted = tldextract.extract(domain)
-
-        # Récupérer uniquement le spfdmarc si le domaine principal n'est pas déjà présent
-        domain_main = f"{extracted.domain}.{extracted.suffix}"
-        #cursor.execute('SELECT spfdmarc FROM domain_details JOIN domains ON domains.id = domain_details.domain_id WHERE domain_name LIKE ?', (f"%.{domain_main}",))
-        #existing_main_domain = cursor.fetchone()
-
-        #if existing_main_domain:
-        #    spfdmarc = existing_main_domain[0]
-        #else:
-        #    spfdmarc = get_spfdmarc(domain_main)  # Récupérer spfdmarc seulement si le domaine principal n'est pas présent
-
-        spfdmarc = get_spfdmarc(domain_main)
- #       if not ip:
- #           http_status = None
- #           techno = None
- #           open_ports = None
- #           screenshot = None
- #           title = None
- #           phash = None
- #           method = None
-#        else:
-        http_status = get_http_status(domain)
-        techno = get_techno(domain)
-        open_ports = scan_naabu_fingerprint(domain)
-        screenshot = take_screenshot(domain)
-        title = get_title(domain)
-        phash = get_phash(screenshot)
-        method = get_method(domain)
-        #print(get_method(domain))
-
-        # Récupérer l'ID du programme
-        cursor.execute('SELECT id FROM programs WHERE program_name = ?', (program_name,))
-        program_id = cursor.fetchone()
-
-        if program_id:
-            # Ajouter le domaine dans la table domains
-            cursor.execute('''
-                INSERT INTO domains (program_id, domain_name) VALUES (?, ?)
-            ''', (program_id[0], domain))
-            domain_id = cursor.lastrowid
-
-            # Ajouter les détails du domaine dans la table domain_details
-            cursor.execute('''
-                INSERT INTO domain_details (domain_id, title, ip, http_status, techno, open_port, screen, phash, spfdmarc, method)
-                VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?)
-            ''', (domain_id, title, ip, http_status, techno, open_ports, screenshot, phash, spfdmarc, method))
-
-            conn.commit()
-            #print(f"✔️ Domain {domain} added successfully.")
-        else:
-            print(f"❌ Program {program_name} not found.")
-
-    except Exception as e:
-        print(f"❌ Failed to add domain {domain}: {e}")
-    finally:
-        cursor.close()
-        conn.close()
+# Fonction principale pour démarrer le traitement
+def maintest(domains, program_name):
+    end=get_httpx_data(domains)
+    update_db(program_name,end)
